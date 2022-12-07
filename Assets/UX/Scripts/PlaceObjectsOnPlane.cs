e9533afa88fca7d67a4408a52c857b01839a4856
PROCW <developerteam888@outlook.com>
2022-12-07 08:13:43 -0800

    finish

---
+++ Assets/UX/Scripts/PlaceObjectsOnPlane.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceObjectsOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_PlacedPrefab;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public GameObject placedPrefab
+    {
+        get { return m_PlacedPrefab; }
+        set { m_PlacedPrefab = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+    public Camera camera; 
+    
+    public enum CubeFace {
+        Left,
+        Bottom,
+        Back,
+        Right,
+        Top,
+        Front,
+    }
+    /// <summary>
+    /// Invoked whenever an object is placed in on a plane.
+    /// </summary>
+    public static event Action onPlacedObject;
+
+    ARRaycastManager m_RaycastManager;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+    
+    [SerializeField]
+    int m_MaxNumberOfObjectsToPlace = 1;
+
+    int m_NumberOfPlacedObjects = 0;
+
+    [SerializeField]
+    bool m_CanReposition = true;
+
+    public bool canReposition
+    {
+        get => m_CanReposition;
+        set => m_CanReposition = value;
+    }
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = s_Hits[0].pose;
+
+                    if (m_NumberOfPlacedObjects < m_MaxNumberOfObjectsToPlace)
+                    {
+                        spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                        GetFaceToward(spawnedObject, camera.transform.position);
+                        m_NumberOfPlacedObjects++;
+                    }
+                    else
+                    {
+                        if (m_CanReposition)
+                        {
+                            spawnedObject.transform.SetPositionAndRotation(hitPose.position, hitPose.rotation);
+                            GetFaceToward(spawnedObject, camera.transform.position);
+                        }
+                    }
+                    
+                    if (onPlacedObject != null)
+                    {
+                        onPlacedObject();
+                    }
+                }
+            }
+        }
+        if(spawnedObject != null) {
+            GetFaceToward(spawnedObject, camera.transform.position);
+        }
+    }
+    /// <summary>
+    /// Change the Color of Cube Object
+    /// </summary>
+    void ChangeTexture(GameObject parent, CubeFace face) {
+        GameObject cube = parent.transform.Find("Cube").gameObject;
+        GameObject face_bottom = cube.transform.Find("Bottom").gameObject;
+        GameObject face_top = cube.transform.Find("Top").gameObject;
+        GameObject face_left = cube.transform.Find("Left").gameObject;
+        GameObject face_front = cube.transform.Find("Front").gameObject;
+        GameObject face_right = cube.transform.Find("Right").gameObject;
+        GameObject face_back = cube.transform.Find("Back").gameObject;
+        face_front.GetComponent<Renderer>().material.color = Color.blue;
+        face_right.GetComponent<Renderer>().material.color = Color.blue;
+        face_left.GetComponent<Renderer>().material.color = Color.blue;           
+        face_back.GetComponent<Renderer>().material.color = Color.blue;
+        face_top.GetComponent<Renderer>().material.color = Color.blue;          
+        face_bottom.GetComponent<Renderer>().material.color = Color.blue;
+        switch(face) {
+            case CubeFace.Left: 
+                face_left.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case CubeFace.Bottom: 
+                face_bottom.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case CubeFace.Back: 
+                face_back.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case CubeFace.Right: 
+                face_right.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case CubeFace.Top: 
+                face_top.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            case CubeFace.Front: 
+                face_front.GetComponent<Renderer>().material.color = Color.red;
+                break;
+            default: break;
+        }    
+    }
+    /// <summary>
+    /// Realtime Call this function for getting the closed face of Cube from Camera
+    /// </summary>
+    void GetFaceToward(GameObject cube, Vector3 observerPosition) {
+        var toObserver = cube.transform.InverseTransformPoint(observerPosition);
+        var absolute = new Vector3(
+                        Mathf.Abs(toObserver.x),
+                        Mathf.Abs(toObserver.y),
+                        Mathf.Abs(toObserver.z)
+                    );                          
+        if (absolute.x >= absolute.y) {
+            if (absolute.x >= absolute.z) {
+                ChangeTexture(cube, toObserver.x > 0 ? CubeFace.Right : CubeFace.Left);
+                return;
+            } else {
+                ChangeTexture(cube, toObserver.x > 0 ? CubeFace.Front : CubeFace.Back);
+                return;
+            }
+        } else if (absolute.y >= absolute.z) {
+            ChangeTexture(cube, toObserver.y > 0 ? CubeFace.Top : CubeFace.Bottom);
+            return;
+        } else {
+            ChangeTexture(cube, toObserver.x > 0 ? CubeFace.Front : CubeFace.Back);
+            return;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # AR_Foundation using Unity3D
 # Tested with Android
 # Possible to detecting the distance to faces from device's camera
+1. Implemented the ARPlane feature with ARPlaneManager 
+The plane manager is a type of trackable manager.
+The plane manager creates GameObjects for each detected plane in the environment. A plane is a flat surface represented by a pose, dimensions, and boundary points. The boundary points are convex.
+
+Examples of features in the environment that can be detected as planes are horizontal tables, floors, countertops, and vertical walls.
+
+You can specify a detection mode, which can be horizontal, vertical, or both. Some platforms require extra work to perform vertical plane detection, so if you only need horizontal planes, you should disable vertical plane detection.
+
+2. Implemented the ARPointCloud feature with ARPointCloudManager
+The point cloud manager is a type of trackable manager.
+The point cloud manager creates point clouds, which are sets of feature points. A feature point is a specific point in the point cloud which the device uses to determine its location in the world. Feature points are typically notable features in the environment that the device can track between frames, such as a knot in a wooden table.
+
+A point cloud is a set of feature points that can change from frame to frame. Some platforms only produce one point cloud, while others organize their feature points into different point clouds in different areas of space.
+
+A point cloud is considered a trackable, while individual feature points are not. However, feature points can be uniquely identified between frames as they have unique identifiers.
+
+3. Implemented the Place Object on plane with PlaceObjectOnPlane
+When click the screen after detect the plane, display the Cube object
